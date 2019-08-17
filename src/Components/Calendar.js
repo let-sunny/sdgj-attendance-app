b999e3a5ebb4e190bc813a27eb62144d73360b01
Sunny <sunny.dev.js@gmail.com>
2019-08-17 23:05:09 +0900

    modified style

--- src/Components/Calendar.js
+++ src/Components/Calendar.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import moment from 'moment'
 import 'moment-lunar'
-moment.updateLocale('kr', {
-	weekdaysShort : ['일', '월', '화', '수', '목', '금', '토']
-});
+import Logo from '../sdgj_horizontal_logo.png';
 
 class Calendar extends React.Component {
 	state = {
 		dateObject: moment(),
-		selectedDay: null
+		selectedDay: null,
+		touched: false,
+		startX: null,
+		currentX: null
 	};
 
 	weekdayShort = moment.weekdaysShort();
@@ -22,10 +23,6 @@ class Calendar extends React.Component {
 
 	daysInMonth = () => {
 		return this.state.dateObject.daysInMonth();
-	};
-
-	today = () => {
-		return this.state.dateObject.format('D');
 	};
 
 	month = () => {
@@ -56,6 +53,42 @@ class Calendar extends React.Component {
 		});
 	};
 
+	onTouchMove = (e) => {
+		if (this.state.touched) {
+			this.setState({
+				currentX: e.targetTouches[0].clientX
+			});
+		} else {
+			this.setState({
+				startX: e.targetTouches[0].clientX,
+				touched: true
+			});
+		}
+	};
+
+	onTouchEnd = () => {
+		const { currentX, startX } = this.state;
+		if (Math.abs(currentX - startX ) > 130) {
+			currentX - startX > 0 ? this.onPrev() : this.onNext();
+		}
+		this.setState({
+			touched: false,
+			currentX: null,
+			startX: null
+		});
+	};
+
+	isToday = (d) => {
+		const { dateObject } = this.state;
+		const day = moment({
+			years: dateObject.format('Y'),
+			months: Number(dateObject.format('MM'))-1,
+			date: d
+		});
+		const today = moment().format('YYYY-MM-DD');
+		return !!day.isSame(today)
+	};
+
 	isSunday = (d) => {
 		const { dateObject } = this.state;
 		const day = moment({
@@ -64,6 +97,16 @@ class Calendar extends React.Component {
 			date: d
 		}).day();
 		return day === 0;
+	};
+
+	isSaturday = (d) => {
+		const { dateObject } = this.state;
+		const day = moment({
+			years: dateObject.format('Y'),
+			months: Number(dateObject.format('MM'))-1,
+			date: d
+		}).day();
+		return day === 6;
 	};
 
 	isHoliday = (d) => {
@@ -105,9 +148,38 @@ class Calendar extends React.Component {
 	};
 
 	render() {
-		let weekdaysName = this.weekdayShort.map(day => {
+		let weekdaysName = this.weekdayShort.map((day, i) => {
+			let chineseWeekday = '';
+			switch (i) {
+				case 0:
+					chineseWeekday = '日';
+					break;
+				case 1:
+					chineseWeekday = '月';
+					break;
+				case 2:
+					chineseWeekday = '火';
+					break;
+				case 3:
+					chineseWeekday = '水';
+					break;
+				case 4:
+					chineseWeekday = '木';
+					break;
+				case 5:
+					chineseWeekday = '金';
+					break;
+				case 6:
+					chineseWeekday = '土';
+					break;
+				default:
+					break;
+			}
 			return (
-				<th key={day} className="calendar-week-day">{day}</th>
+				<th key={day} className="calendar-week-day">
+					<p className="ch">{chineseWeekday}</p>
+					<p className="en">{day.toUpperCase()}</p>
+				</th>
 			);
 		});
 
@@ -122,8 +194,9 @@ class Calendar extends React.Component {
 
 			// set class
 			const classes = ['calendar-day'];
-			if (Number(day) === Number(this.today())) classes.push('today');
+			if (this.isToday(day)) classes.push('today');
 			if (this.isSunday(day) || this.isHoliday(day)) classes.push('holiday');
+			if (this.isSaturday(day)) classes.push('saturday');
 			return (
 				<td key={day} className={classes.join(' ')}>
 					<button type="button"
@@ -152,18 +225,12 @@ class Calendar extends React.Component {
 		});
 
 		return (
-			<div className="calendar">
+			<div className="calendar"
+			     onTouchMove={(e) => this.onTouchMove(e)}
+			     onTouchEnd={() => this.onTouchEnd()}>
 				<section className="calendar-header">
-					<nav className="calendar-nav">
-						<button onClick={() => {this.onPrev();}}
-						        className="calendar-button button-prev"
-						>{'<'}</button>
-						<button onClick={() => {this.onNext();}}
-						        className="calendar-button button-next"
-						>{'>'}</button>
-					</nav>
 					<div className="calendar-label">
-						<div className="calendar-label-month"><strong>{this.month().toUpperCase()}</strong></div>
+						<div className="calendar-label-month">{this.month().toUpperCase()}</div>
 						<div className="calendar-label-year"> {this.year()}</div>
 					</div>
 				</section>
@@ -176,7 +243,9 @@ class Calendar extends React.Component {
 				</section>
 
 				<section className="calendar-footer">
-					© 2019 시대공작
+					<img src={Logo} alt="logo" />
+					<p className="address">서울 마포구 서교동 358-93</p>
+					<p className="copy-rights">© 2019 시대공작</p>
 				</section>
 			</div>
 		);
